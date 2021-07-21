cf556fdcdd629f9c91c01ab2b72633d609ef9ec3
wengyin777 <wengyin777@gmail.com>
2021-07-22 02:23:48 +0800

    SIMPLE API

---
+++ Second.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"encoding/json"
+)
+
+type Article struct{
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+
+}
+
+var Articles []Article
+
+func homePage(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles",returnAllArticles)
+	log.Fatal(http.ListenAndServe(":10000",nil))
+}
+func returnAllArticles(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
+
+func main() {
+	Articles = []Article{
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
+	handleRequests()
+
+}
